aef1e4d3d7edf0514eb3bcc46c89a748eb2339ee
BuildTools <unconfigured@null.spigotmc.org>
2023-04-07 08:48:21 +0300

    after alot of pain the queen can now move and it can capture othrer pieces

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const blackPieces = document.querySelectorAll('.black')
 const whitePieces = document.querySelectorAll('.white')
 const pawns = document.querySelectorAll('.pawn')
 const tileCodes = {a: 1,b: 2,c: 3,d:4,e:5,f:6,g:7,h:8}
+const tileCodes2 = {1: 'a',2: 'b',3: 'c',4:'d',5:'e',6:'f',7:'g',8:'h'}
 let pieceStartingSquare;
 let isValidCapture = true;
 let pieceStaringSquares = {};
@@ -20,6 +21,7 @@ const turnChange = (piece) => {
       turnCounter++
     }
 }
+
 
 const pawnMovement = (color,tile,piece,ogRow,currentRow,ogcol,currcol) => {
     let mutltiplier = 1
@@ -64,6 +66,85 @@ const kingMovement = (piece,ogRow,currentRow,ogcol,currcol) => {
     return false
   }
 }
+const queenMovement = (piece,ogRow,currentRow,ogcol,currcol) => {
+  let i = 0
+  
+  while(true){
+    if(currcol == ogcol && currentRow == ogRow){
+      pieceStartingSquare.appendChild(piece)
+      return false
+    } else if((currentRow == ogRow + i || currentRow == ogRow - i || currentRow == ogRow) && (currcol == ogcol + i || currcol == ogcol - i || currcol == ogcol)){
+      let thing
+      const rows = ogRow - currentRow
+      const cols = ogcol - currcol
+      if(Math.abs(rows) == Math.abs(cols)){
+        for(let i = 1; i <= Math.abs(rows) - 1; i++){
+          thing = document.querySelectorAll(`.tile-${tileCodes2[currcol + i]}`)
+          arraything = Array.from(thing).reverse()
+          if(cols > 0) {
+            if(rows < 0 ){
+              if(arraything[currentRow - 1 - i].children[0] != undefined){
+                return false
+              }
+            } else {
+              if(arraything[currentRow - 1 + i].children[0] != undefined){
+                return false
+              }
+            }
+          } else {
+              thing = document.querySelectorAll(`.tile-${tileCodes2[currcol - i]}`)
+              arraything = Array.from(thing).reverse();
+            if(rows < 0 ){
+              if(arraything[currentRow - 1 - i].children[0] != undefined){
+                return false
+              }
+              } else {
+              if(arraything[currentRow - 1 + i].children[0] != undefined){
+                return false
+              }
+            }
+          }
+        }
+        return true
+      }
+      for(let i = 0; i <= Math.abs(rows) - 2;i++){
+        if(rows > 0) {
+            thing = document.querySelector(`.row-${currentRow + i + 1}`)
+            if(thing.children[currcol - 1].children[0] != undefined){
+              return false
+            }
+        } else {
+            thing = document.querySelector(`.row-${currentRow - i - 1}`)
+            if(thing.children[currcol - 1].children[0] != undefined){
+              return false
+            }
+        }
+      }
+      for(let i = 0; i <= Math.abs(cols) - 2;i++){
+        if(cols > 0) {
+            thing = document.querySelectorAll(`.tile-${tileCodes2[currcol + i + 1]}`)
+            arraything = Array.from(thing).reverse();
+            if(arraything[currentRow - 1].children[0] != undefined){
+              return false
+            }
+        } else {
+            thing = document.querySelectorAll(`.tile-${tileCodes2[currcol - i - 1]}`)
+            arraything = Array.from(thing).reverse();
+            if(arraything[currentRow - 1].children[0] != undefined){
+              return false
+            }
+        }
+      }
+      return true
+    }
+    if(i > 10){
+      pieceStartingSquare.appendChild(piece)
+      return false
+    } else{
+      i++
+    }
+  }
+}
 
 const checkIfPieceCanMoveToThatTile = (tile, piece) => {
   let originalRowAsNumber = Number(pieceStartingSquare.parentElement.classList[1][pieceStartingSquare.parentElement.classList[1].length - 1])
@@ -76,11 +157,14 @@ const checkIfPieceCanMoveToThatTile = (tile, piece) => {
     } 
     else if(piece.classList.contains('king')){
       return kingMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
-    } 
+    } else if(piece.classList.contains('queen')){
+      return queenMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
+    }
     else return false
   } else {
     if(pawnMovement('black',tile,piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum) && piece.classList.contains('pawn')) return true
     else if(piece.classList.contains('king')) return kingMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
+    else if(piece.classList.contains('queen')) return queenMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
     else return false
   }
 }
@@ -116,6 +200,7 @@ const checkIfYouCanCapture = (tile,piece) => {
     if(piece.classList.contains('pawn')){
       return pawnCaptures('white',piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
     } else if(piece.classList.contains('king')) return kingCaptures(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
+    else if(piece.classList.contains('queen')) return queenMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
     else return false
 
   } else {
@@ -123,6 +208,8 @@ const checkIfYouCanCapture = (tile,piece) => {
       return pawnCaptures('black',piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
     } else if(piece.classList.contains('king')){
       return kingCaptures(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
+    } else if(piece.classList.contains('queen')){
+      return queenMovement(piece,originalRowAsNumber,currentRowAsNumber,originalCollum,currentCollum)
     } else{
       return false
      }
